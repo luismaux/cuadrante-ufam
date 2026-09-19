04b702d162fd563114792086d41aa1d6bd541ade
luismaux <luismaux@gmail.com>
2026-09-20 01:12:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cuadrante-cache-v1";
+const CACHE_NAME = "cuadrante-cache-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
